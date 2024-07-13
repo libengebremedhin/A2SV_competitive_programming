6f95b92d35a527c55dd8bb0841e9bf1a38b54c6a
Liben Gebremedhin <149259472+libengebremedhin@users.noreply.github.com>
2024-07-12 22:31:01 -0700

    Create 1365. How Many Numbers Are Smaller Than the Current Number.py

---
+++ Community Education/Leetcode/1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+
+        num  = sorted(nums)
+        ans = []
+        for n in nums:
+            ans.append(num.index(n))
+        return ans
+        
+
+
+        
